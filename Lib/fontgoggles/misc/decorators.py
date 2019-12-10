d8aa1484624024869c4e5a3a6dbc7c5bf9f09ee6
justvanrossum <justvanrossum@gmail.com>
2019-12-10 16:14:23 +0100

    add asyncTask decorator, to easily create asyncio tasks using a regular function call

--- Lib/fontgoggles/misc/decorators.py
+++ Lib/fontgoggles/misc/decorators.py
@@ -1,5 +1,18 @@
+import asyncio
 import functools
 import logging
+
+
+def asyncTask(func):
+    """Wraps an async function or method into a regular function, that
+    will schedule the async function as a task. Returns an asyncio Task
+    object, that can be cancelled.
+    """
+    @functools.wraps(func)
+    def createFuncTask(*args):
+        coro = func(*args)
+        return asyncio.create_task(coro)
+    return createFuncTask
 
 
 def suppressAndLogException(func):
