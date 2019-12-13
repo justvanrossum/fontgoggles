22e8b5334a845e92e34fe91aed28735464c05407
justvanrossum <justvanrossum@gmail.com>
2019-12-13 14:12:16 +0100

    renamed path arg to fontPath for even more consistency

--- Lib/fontgoggles/font/__init__.py
+++ Lib/fontgoggles/font/__init__.py
@@ -1,40 +1,40 @@
 from os import PathLike
 
 
-def getOpener(path:PathLike):
-    openerKey = sniffFontType(path)
+def getOpener(fontPath:PathLike):
+    openerKey = sniffFontType(fontPath)
     assert openerKey is not None
     numFontsFunc, openerFunc = fontOpeners[openerKey]
     return numFontsFunc, openerFunc
 
 
-def sniffFontType(path:PathLike):
-    if not isinstance(path, PathLike):
-        raise TypeError("path must be a Path(-like) object")
-    assert path.is_file()
-    openerKey = path.suffix.lower().lstrip(".")
+def sniffFontType(fontPath:PathLike):
+    if not isinstance(fontPath, PathLike):
+        raise TypeError("fontPath must be a Path(-like) object")
+    assert fontPath.is_file()
+    openerKey = fontPath.suffix.lower().lstrip(".")
     if openerKey not in fontOpeners:
         return None
     return openerKey
 
 
-async def openOTF(path:PathLike, fontNumber:int, fontData=None):
+async def openOTF(fontPath:PathLike, fontNumber:int, fontData=None):
     from .baseFont import OTFFont
     if fontData is not None:
         font = OTFFont(fontData, fontNumber)
     else:
-        font = OTFFont.fromPath(path, fontNumber)
+        font = OTFFont.fromPath(fontPath, fontNumber)
         fontData = font.fontData
     return (font, fontData)
 
 
-def numFontsOTF(path:PathLike):
+def numFontsOTF(fontPath:PathLike):
     return 1
 
 
-def numFontsTTC(path:PathLike):
+def numFontsTTC(fontPath:PathLike):
     from fontTools.ttLib.sfnt import readTTCHeader
-    with open(path, "rb") as f:
+    with open(fontPath, "rb") as f:
         header = readTTCHeader(f)
     return header.numFonts
 
