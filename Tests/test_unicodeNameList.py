e3f40ce9af3a1b92d1e1840ab0ec62fd7f93a07d
justvanrossum <justvanrossum@gmail.com>
2019-12-17 12:36:39 +0100

    test the unicode name list

---
+++ Tests/test_unicodeNameList.py
@@ -0,0 +1,21 @@
+import pytest
+from fontgoggles.misc.unicodeNameList import findPrefix
+
+
+testData = [
+    ("ROOTS", []),
+    ("ROOT", [1542, 1543, 8730, 8731, 8732]),
+    ("ROO", 12),
+    ("RO", 352),
+    ("R", 3312),
+    ("", 131259),
+]
+
+@pytest.mark.parametrize("prefix,expectedChars", testData)
+def test_findPrefix(prefix, expectedChars):
+    chars = findPrefix(prefix)
+    if isinstance(expectedChars, int):
+        assert len(chars) == expectedChars
+    else:
+        assert chars == expectedChars
+    assert sorted(chars) == chars
