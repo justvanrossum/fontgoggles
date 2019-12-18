fefeb16e128f50e486cc87987fbd552594d45b91
justvanrossum <justvanrossum@gmail.com>
2019-12-18 14:05:44 +0100

    make alignment work better when the scrollview resizes

--- Lib/fontgoggles/mac/aligningScrollView.py
+++ Lib/fontgoggles/mac/aligningScrollView.py
@@ -59,17 +59,27 @@ class _AligningScrollView_ClipView(AppKit.NSClipView):
     def __init__(self, hAlign, vAlign):
         self.hAlign = hAlign
         self.vAlign = vAlign
+        self._prevBounds = self.bounds()
 
     def mouseDown_(self, event):
         # A click occured in the clipview, but outside the document view. Happens when it is
         # smaller than the clipview. Make the document view first responder.
         self.window().makeFirstResponder_(self.documentView())
 
+
     def constrainBoundsRect_(self, proposedClipViewBoundsRect):
         # Partially taken from https://stackoverflow.com/questions/22072105/
         rect = super().constrainBoundsRect_(proposedClipViewBoundsRect)
         view = self.documentView()
-        viewFrame = view.frame()
+        viewFrame = view.bounds()
+
+        if self._prevBounds is not None:
+            dx = self.bounds().origin.x - self._prevBounds.origin.x
+            dy = self.bounds().origin.y - self._prevBounds.origin.y
+            dw = self.bounds().size.width - self._prevBounds.size.width
+            dh = self.bounds().size.height - self._prevBounds.size.height
+        else:
+            dx = dy = dw = dh = 0
 
         # TODO: this does not work well together with magnification, at least not
         # When the document view is a vanilla group.
@@ -82,6 +92,11 @@ class _AligningScrollView_ClipView(AppKit.NSClipView):
                     rect.origin.x = 0
                 elif self.hAlign == "right":
                     rect.origin.x = (viewFrame.size.width - rect.size.width)
+            elif self._prevBounds is not None:
+                if self.hAlign == "center":
+                    rect.origin.x = self._prevBounds.origin.x - dw / 2 + dx
+                elif self.hAlign == "right":
+                    rect.origin.x = self._prevBounds.origin.x - dw + dx
 
             if rect.size.height > viewFrame.size.height:
                 if view.isFlipped():
@@ -99,4 +114,5 @@ class _AligningScrollView_ClipView(AppKit.NSClipView):
                     elif self.vAlign == "bottom":
                         rect.origin.y = -(viewFrame.size.height - rect.size.height)
 
+        self._prevBounds = rect
         return rect
