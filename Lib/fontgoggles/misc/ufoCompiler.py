5eea28e0c5b3f42538431f99a0d9607e2b2c3343
justvanrossum <justvanrossum@gmail.com>
2020-01-27 10:14:57 +0100

    make main func return a TTFont instead of data

--- Lib/fontgoggles/misc/ufoCompiler.py
+++ Lib/fontgoggles/misc/ufoCompiler.py
@@ -18,10 +18,14 @@ def compileMinimumFont_captureOutput(ufoPath):
     f = io.StringIO()
     with redirect_stdout(f), redirect_stderr(f):
         try:
-            data, error = compileMinimumFont(ufoPath)
+            ttFont, error = compileMinimumFont(ufoPath)
         except Exception:
             data = None
             error = traceback.format_exc()
+        else:
+            f = io.BytesIO()
+            ttFont.save(f, reorderTables=False)
+            data = f.getvalue()
     return data, f.getvalue(), error
 
 
@@ -58,9 +62,7 @@ def compileMinimumFont(ufoPath):
         error = traceback.format_exc()
     else:
         error = None
-    f = io.BytesIO()
-    ttFont.save(f, reorderTables=False)
-    return f.getvalue(), error
+    return ttFont, error
 
 
 class UFOInfo:
