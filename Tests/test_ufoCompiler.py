a364ac8e0d6afb896c7c02b1c846eead0b71bd4a
justvanrossum <justvanrossum@gmail.com>
2020-02-02 14:47:20 +0100

    fixed expected results

--- Tests/test_ufoCompiler.py
+++ Tests/test_ufoCompiler.py
@@ -50,5 +50,5 @@ async def test_compileUFOToPathMultiple(tmpdir):
     coros = (compileUFOToPath(u, t, output.append) for u, t in zip(ufoPaths, ttPaths))
     results = await asyncio.gather(*coros)
     assert len(results) == len(ufoPaths)
-    assert results == [False] * len(results)
+    assert results == [None] * len(results)
     assert [(os.stat(p).st_size > 0) for p in ttPaths] == [True] * len(results)
