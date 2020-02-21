84bd7045bb942bc8aab4d82f5ed14e6bf3ed4ec2
justvanrossum <justvanrossum@gmail.com>
2020-02-21 20:08:13 +0100

    set verbose so we see more tracebacks

--- App/FontGoggles.py
+++ App/FontGoggles.py
@@ -1,5 +1,6 @@
 import asyncio
 from corefoundationasyncio import CoreFoundationEventLoop
+import objc
 
 # Make sure these classes are loaded
 from fontgoggles.mac.document import FGDocument  # noqa: F401
@@ -7,6 +8,7 @@ from fontgoggles.mac.appDelegate import FGAppDelegate  # noqa: F401
 
 
 if __name__ == "__main__":
+    objc.setVerbose(1)
     loop = CoreFoundationEventLoop()
     asyncio.set_event_loop(loop)
     try:
