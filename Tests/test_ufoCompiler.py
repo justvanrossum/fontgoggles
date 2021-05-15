e1a721c6f6a0b99c5d832a5442cab219ab4215fe
justvanrossum <justvanrossum@gmail.com>
2021-05-15 13:06:40 +0200

    formatting

--- Tests/test_ufoCompiler.py
+++ Tests/test_ufoCompiler.py
@@ -16,7 +16,11 @@ def test_ufoCharacterMapping():
     # MutatorSansBoldWideMutated.ufo/glyphs/A_.glif contains a commented-out <unicode>
     # tag, that must not be parsed, as well as a commented-out <anchor>.
     assert 0x1234 not in cmap
-    assert anchors == {"A": [("top", 645, 815)], "E": [("top", 582.5, 815)], "macroncmb": [("_top", 0, 815)]}
+    assert anchors == {
+        "A": [("top", 645, 815)],
+        "E": [("top", 582.5, 815)],
+        "macroncmb": [("_top", 0, 815)],
+    }
 
 
 def test_ufoCharacterMapping_glyphNames():
