f6de7b3d21eb8f0e54c5b80c987f0c72433d9583
justvanrossum <justvanrossum@gmail.com>
2021-05-26 22:08:38 +0200

    add COLRv1 support via blackrenderer

--- Lib/fontgoggles/font/glyphDrawing.py
+++ Lib/fontgoggles/font/glyphDrawing.py
@@ -1,3 +1,4 @@
+from AppKit import NSGraphicsContext
 from fontTools.misc.arrayTools import unionRect
 from ..mac.drawing import rectFromNSRect
 from ..misc.properties import cachedProperty
@@ -60,3 +61,22 @@ class GlyphLayersDrawing:
 
     def pointInside(self, pt):
         return any(path.containsPoint_(pt) for path, colorID in self.layers)
+
+
+class GlyphCOLRv1Drawing:
+    def __init__(self, glyphName, colorFont):
+        self.glyphName = glyphName
+        self.colorFont = colorFont
+
+    @cachedProperty
+    def bounds(self):
+        return self.colorFont.getGlyphBounds(self.glyphName)
+
+    def draw(self, colorPalette, defaultColor):
+        from blackrenderer.backends.coregraphics import CoreGraphicsCanvas
+
+        cgContext = NSGraphicsContext.currentContext().CGContext()
+        self.colorFont.drawGlyph(self.glyphName, CoreGraphicsCanvas(cgContext))
+
+    def pointInside(self, pt):
+        return False  # TODO: implement
