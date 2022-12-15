2563c7319bf26b598aa07816ab1118082bd73b00
Johannes Neumeier <hello@johannesneumeier.com>
2022-12-15 16:16:21 +0200

    Removed arm64e architecture from build

--- Turbo/build_lib.sh
+++ Turbo/build_lib.sh
@@ -7,6 +7,6 @@ cd "${0%/*}"  # cd into the folder containing this script
 
 mkdir -p build
 
-cc -g -fPIC -c -mmacosx-version-min=10.9 -arch x86_64 -arch arm64e -arch arm64 -o build/makePathFromOutline.o makePathFromOutline.m
+cc -g -fPIC -c -mmacosx-version-min=10.9 -arch x86_64 -arch arm64 -o build/makePathFromOutline.o makePathFromOutline.m
 
-cc -dynamiclib -mmacosx-version-min=10.9 -o ../Lib/fontgoggles/mac/libmakePathFromOutline.dylib -framework AppKit -arch x86_64 -arch arm64e -arch arm64 -lsystem.b build/makePathFromOutline.o
+cc -dynamiclib -mmacosx-version-min=10.9 -o ../Lib/fontgoggles/mac/libmakePathFromOutline.dylib -framework AppKit -arch x86_64 -arch arm64 -lsystem.b build/makePathFromOutline.o
