c5d4e18ea3b2ac9e23ad70935d78465cc69bd81e
Rob Stenson <rob.stenson@gmail.com>
2024-10-13 15:27:24 -0700

    fontgoggles.misc.plotter as abstraction above direct use of CocoaPen (wip; only CocoaPen has been abstracted, not makePathFromArrays)

--- Lib/fontgoggles/font/ufoFont.py
+++ Lib/fontgoggles/font/ufoFont.py
@@ -10,7 +10,6 @@ from fontTools.feaLib.parser import Parser as FeatureParser
 from fontTools.feaLib.ast import IncludeStatement
 from fontTools.feaLib.error import FeatureLibError
 from fontTools.fontBuilder import FontBuilder
-from fontTools.pens.cocoaPen import CocoaPen  # TODO: factor out mac-specific code
 from fontTools.ttLib import TTFont
 from fontTools.ufoLib import UFOReader, UFOFileStructure
 from fontTools.ufoLib import (FONTINFO_FILENAME, GROUPS_FILENAME, KERNING_FILENAME,
@@ -23,6 +22,7 @@ from ..compile.compilerPool import compileUFOToBytes
 from ..compile.ufoCompiler import fetchGlyphInfo
 from ..misc.hbShape import HBShape
 from ..misc.properties import cachedProperty
+from ..misc.plotter import Plotter
 
 
 class UFOFont(BaseFont):
@@ -151,9 +151,9 @@ class UFOFont(BaseFont):
         return glyph
 
     def _addOutlinePathToGlyph(self, glyph):
-        pen = CocoaPen(self.glyphSet)
-        glyph.draw(pen)
-        glyph.outline = pen.path
+        plotter = Plotter(self.glyphSet)
+        glyph.draw(plotter.pen)
+        glyph.outline = plotter.getOutline()
 
     def _getHorizontalAdvance(self, glyphName):
         glyph = self._getGlyph(glyphName)
@@ -258,9 +258,9 @@ class NotDefGlyph:
         pass
 
     def getOutline(self):
-        pen = CocoaPen(None)  # by now there are no more composites
-        self.draw(pen)
-        return pen.path
+        plotter = Plotter(None)
+        self.draw(plotter.pen)
+        return plotter.getOutline()
 
 
 class Glyph(GLIFGlyph):
--- Tests/test_makeOutline.py
+++ Tests/test_makeOutline.py
@@ -1,8 +1,8 @@
 import sys
 import pytest
-from fontTools.pens.cocoaPen import CocoaPen
 from fontTools.ttLib import TTFont
 from fontgoggles.font.otfFont import OTFFont
+from fontgoggles.misc.plotter import Plotter
 from testSupport import getFontPath
 
 
@@ -19,12 +19,12 @@ async def test_getOutlinePath():
 
     for glyphName in ["a", "B", "O", "period", "bar", "aring"]:
         p = font._getGlyphOutline(glyphName)
-        pen = CocoaPen(ttfGlyphSet)
-        ttfGlyphSet[glyphName].draw(pen)
+        plotter = Plotter(ttfGlyphSet)
+        ttfGlyphSet[glyphName].draw(plotter.pen)
         # The paths are not identical, due to different rounding
         # of the implied points, and different closepath behavior,
         # so comparing is hard, so we'll settle for a bounding box.
-        assert p.controlPointBounds() == pen.path.controlPointBounds()
+        assert p.controlPointBounds() == plotter.pen.path.controlPointBounds()
 
 
 @pytest.mark.asyncio
